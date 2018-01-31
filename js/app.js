7e230f170c56648be9be268db79a3debbf04a07e
arintaosorio <arintaosorio@gmail.com>
2018-01-31 08:15:56 -0600

    link google

--- js/app.js
+++ js/app.js
@@ -33,23 +33,17 @@ function authGoogle(){
 
 function authentication(provider){
   firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
-    // The signed-in user info.
     var user = result.user;
-    localStorage.setItem('userName',user.displayName);
-    localStorage.setItem('userPhoto', user.photoURL);
+    
     window.location.href = 'views/filtro-1.html';
-    // ...
+
   }).catch(function(error) {
-    // Handle Errors here.
+
     var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
-    // ...
+
 });
 
 };
